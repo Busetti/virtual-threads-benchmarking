38260543fd8d8af4c23016514a2b529f7561e0db
Busetti <guru.svu31@gmail.com>
2024-07-06 19:11:02 +0530

    first commit

---
+++ src/main/java/com/guru/virtual_threads_demo/VirtualThreadsDemo.java
@@ -0,0 +1,13 @@
+package com.guru.virtual_threads_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadsDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualThreadsDemo.class, args);
+	}
+
+}
